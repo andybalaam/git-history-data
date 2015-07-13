e8eb54bc70e9c05f80be1179a1e1a648994adb8a
Andy Balaam <andybalaam@artificialworlds.net>
2015-07-13 18:11:26 +0100

    Rename private variables to start with _

--- githistorydata/git.py
+++ githistorydata/git.py
@@ -17,7 +17,7 @@ class Git( object ):
         Return a list of LogLine for the repo in the current dir.
         """
         return list(
-            self.logline( ln.strip() )
+            self._logline( ln.strip() )
             for ln in self.raw_git.git_log_pretty_tformat_H_ai_an()
             if ln.strip() != ""
         )
@@ -29,12 +29,12 @@ class Git( object ):
             list( self._showline( l ) for l in show_lines[1:] if l != "")
         )
 
-    logline_re = re.compile(
+    _logline_re = re.compile(
         r"([0-9a-f]{40}) (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} [-+]\d{4}) (.+)"
     )
 
-    def logline( self, ln ):
-        m = Git.logline_re.match( ln )
+    def _logline( self, ln ):
+        m = Git._logline_re.match( ln )
         if not m:
             raise Exception(
                 "Line from git log '%s' did not match expected format"
